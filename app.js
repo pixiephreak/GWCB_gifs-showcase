1d6dd6435a0bdf81a73593b417173bc37f543698
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2017-03-21 00:13:19 -0400

    use secure request in queryUrl

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ $(document).on('click', '.button', function(){
 	console.log('click');
 	var person = $(this).attr("data-person");
 	//api enpoint concatenated with the celeb
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
 	  person + "&api_key=dc6zaTOxFJmzC&limit=12";
 	  //call
 	$.ajax({
